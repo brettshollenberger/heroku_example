556229fb95f55504c641f06a8c7091113b7e8a77
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-06-13 16:43:35 -0400

    Add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 HerokuExample::Application.routes.draw do
+
+  resources :users
+
+  root to: "users#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
